d10fc82d3aaa0445882e45cf4b983a92a50fc32a
Furkan <68143015+simsekfurkann07@users.noreply.github.com>
2022-11-23 21:19:07 +0300

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -4,7 +4,7 @@ export default function handler(req, res) {
   let { skip } = req.query;
   skip = parseInt(skip) || 0;
 
-  const filter = data.filter((todo) => todo.id > skip && todo.id <= skip + 40);
+  const filter = data.filter((todo) => todo.id > skip && todo.id <= skip + 20); 
 
   res.status(200).json(filter);
 }
